3987f7fffc748802d226a060653cd942a890bbca
Maria Garmash <garmash.masha.et-301@mail.ru>
2024-02-16 23:27:26 +0300

    making homework

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,10 +1,57 @@
-﻿int n = 100;
-int i = 1;
+﻿// int n = 100;
+// int i = 1;
 
-while (i <= n)
-{
-   // Console.WriteLine(i);
-    Console.Write(i);
-    Console.Write(' ');
-    i = i + 1;
-}
+// while (i <= n)
+// {
+//    // Console.WriteLine(i);
+//     Console.Write(i);
+//     Console.Write(' ');
+//     i = i + 1;
+// }
+
+
+// int a;
+// int b;
+
+// a = 13;
+// b = 32;
+
+//   if (a > b){
+//   Console.WriteLine($"первое число больше");
+//   }
+//   if (a == b){
+//     Console.WriteLine($"числа равны");
+//   }
+//   if (a < b){
+//   Console.WriteLine($"второе число больше");
+//   }
+
+
+
+    {
+        Console.WriteLine("Введите три числа:");
+
+        // Ввод трех чисел с клавиатуры
+        Console.Write("Первое число: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Второе число: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Третье число: ");
+        int number3 = int.Parse(Console.ReadLine());
+
+        // Нахождение максимального числа
+        int maxNumber = FindMax(number1, number2, number3);
+
+        // Вывод результата
+        Console.WriteLine($"Максимальное число: {maxNumber}");
+
+        Console.ReadLine(); // Чтобы консольное окно не закрывалось сразу
+    }
+
+    static int FindMax(int a, int b, int c)
+    {
+        // Используем встроенную функцию Math.Max для нахождения максимального числа
+        return Math.Max(Math.Max(a, b), c);
+    }
---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = 27;
+int i = 0;
+while (i < number) {
+  
+    i = i + 1; 
+    if (i %2 == 0) {
+    Console.Write($"{i} ");
+  }
+}
